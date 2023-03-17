02fb39bb9ee86b4e327a7020b8ee4df04cc6a1c5
Kauan Ribeiro Teruel <114168887+KauanRTe@users.noreply.github.com>
2023-03-17 11:38:46 -0300

    Adicionar arquivos de projeto.

---
+++ WIS_PI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WIS_PI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
