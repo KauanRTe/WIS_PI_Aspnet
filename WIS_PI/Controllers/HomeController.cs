3d75aaf4e8bbeff3b4c174fd4c24ace25b5fb518
Kauan Ribeiro Teruel <114168887+KauanRTe@users.noreply.github.com>
2023-04-05 11:40:26 -0300

    Atualização 05/04

--- WIS_PI/Controllers/HomeController.cs
+++ WIS_PI/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace WIS_PI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Praticas()
+        {
+            return View();
+        }
     }
 }
